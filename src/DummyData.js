545d6e2fb702f4514bc6ca6cff010c0610f1b162
lion-rion <lionact08@gmail.com>
2023-03-12 23:47:37 +0900

    [add] dummydata

---
+++ src/DummyData.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const  DummyData =  
+  {
+    id: 1,
+    title: "📝今からやること",
+    tasks: [
+      {
+        host: 1,
+      },
+      {
+        name: "sleep",
+        time: 900,
+      },
+      {
+        id: 1,
+        title: "散歩",
+      },
+    ],
+  };
+
+export default DummyData
