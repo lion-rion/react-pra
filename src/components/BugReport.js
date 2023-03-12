fe3559b7b82bf756ae8879515130cb4d83e6d81a
lion-rion <lionact08@gmail.com>
2023-03-13 00:52:58 +0900

    add component

---
+++ src/components/BugReport.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const BugReport = () => {
+  return (
+    <>
+      <section class="my-10">
+        <div class="py-8 px-4 px-sm-6 px-lg-7 px-xl-10 rounded border bg-slate-50">
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default BugReport
