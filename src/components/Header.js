b819806595e30ed7750be738c1840d71e12c41d0
lion-rion <lionact08@gmail.com>
2023-03-12 21:28:11 +0900

    change nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,8 @@ const Header = () => {
       <nav class="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
         <a class="flex-none text-xl font-semibold dark:text-white" href="#">Ansibler</a>
         <div class="flex flex-row items-center gap-5 mt-5 sm:justify-end sm:mt-0 sm:pl-5">
-          <a class="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500" href="#">Help</a>
-          <a class="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500" href="#">Work</a>
-          <a class="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500" href="#">Blog</a>
+          <a class="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500" href="#">使い方</a>
+          <a class="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500" href="#">バグ報告</a>
         </div>
       </nav>
     </header>
