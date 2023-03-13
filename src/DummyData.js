5d99c40f6ef64abf1bf2d90ebbdb8bec8796a0ae
lion-rion <lionact08@gmail.com>
2023-03-14 01:15:43 +0900

    uuidを追加

--- src/DummyData.js
+++ src/DummyData.js
@@ -1,60 +1,95 @@
+import { v4 as uuidv4 } from "uuid";
 
 const DummyData = [
   {
+    id: uuidv4(),
     name: "✏️ 入力スペース",
     tasks: [
       {
+        id: uuidv4(),
+        tag: "1",
+        icon: "fa-power-off",
+        apt: {
+        name: "apache2",
+        state: "present"
+      }
+        
+      },
+      {
+        id: uuidv4(),
+        icon: "fa-power-off",
         name: "b",
-        title: "散歩",
+        title: '111',
       },
     ],
   },
   {
+    id: uuidv4(),
     name: "⚠️ 電源操作",
     tasks: [
       {
+        id: uuidv4(),
+        icon: "faPowerOff",
         name: "d",
         title: "読書",
       },
       {
+        id: uuidv4(),
+        icon: "faPowerOff",
         name: "e",
         title: "aaa",
       },
       {
+        id: uuidv4(),
+        icon: "faPowerOff",
         name: "f",
         title: "bbb",
       },
     ],
   },
   {
+    id: uuidv4(),
     name: "⚡️ インストール",
     tasks: [
       {
+        id: uuidv4(),
+        icon: "faPowerOff",
         name: "d",
         title: "読書",
       },
       {
+        id: uuidv4(),
+        icon: "faPowerOff",
         name: "e",
         title: "aaa",
       },
       {
+        id: uuidv4(),
+        icon: "faPowerOff",
         name: "f",
         title: "bbb",
       },
     ],
   },
   {
+    id: uuidv4(),
     name: "⚙️ 設定",
     tasks: [
       {
+        id: uuidv4(),
+        icon: "faPowerOff",
         name: "d",
         title: "読書",
       },
       {
+        id: uuidv4(),
+        icon: "faPowerOff",
         name: "e",
         title: "aaa",
       },
       {
+        id: uuidv4(),
+        icon: "faPowerOff",
         name: "f",
         title: "bbb",
       },
